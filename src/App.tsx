b6193375a2f6b83bc8dbdfec3f92837d04527258
wellmota <impeto.wellington@gmail.com>
2024-09-28 22:22:05 -0300

    Refactor file imports and remove unused components

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "styled-components"
 import { BrowserRouter } from "react-router-dom"
 import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from "./styles/global"
-import { Router } from "./components/Router"
+import { Router } from "./Router"
 
 export function App() {
   return (
