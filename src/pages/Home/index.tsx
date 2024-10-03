7fc237646f7333245ccae57e7ddc40aefc192f96
wellmota <impeto.wellington@gmail.com>
2024-10-03 11:06:15 -0300

    Refactor Home page component and styles, add task input suggestions and improve minutes input

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,13 +15,41 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">I'll work on</label>
-          <TaskInput id="task" placeholder="Name your project" />
+          <TaskInput
+            id="task"
+            list="tasks-suggestions"
+            placeholder="Name your project"
+          />
+
+          <datalist id="tasks-suggestions">
+            <option value="Front-end project" />
+            <option value="Back-end project" />
+            <option value="Mobile project" />
+            <option value="Design project" />
+            <option value="Marketing project" />
+            <option value="Research project" />
+            <option value="Documentation project" />
+            <option value="Testing project" />
+            <option value="Deployment project" />
+            <option value="Maintenance project" />
+            <option value="Support project" />
+            <option value="Meeting project" />
+            <option value="Training project" />
+            <option value="Learning project" />
+            <option value="Personal project" />
+            <option value="Open-source project" />
+            <option value="Community project" />
+            <option value="Other project" />
+          </datalist>
 
           <label htmlFor="minutesAmount">for</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
           <span>minutes.</span>
         </FormContainer>
